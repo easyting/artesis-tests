bed728b6d940410bf9ee27b54680da633ccb3208
Alex Semeniuc <as@inlead.dk>
2013-03-18 17:59:45 +0200

    Added exit codes for shell scripts.

--- ci.sh
+++ ci.sh
@@ -6,5 +6,7 @@ INSTANCE="artesis-l-${BUILD_NUMBER}"
 ## Local vars
 SELENIUM_BROWSER_URL=http://$INSTANCE.ci.inlead.dk
 
-## Run tests
-exec /usr/bin/env SELENIUM_BROWSER_URL=${SELENIUM_BROWSER_URL} ./run.sh --log-junit=logs/junit-${BUILD_NUMBER}.xml --testdox-html=logs/testdox-${BUILD_NUMBER}.html "$@"
+## Run tests 
+## Will exit with code 1 on failure, 2 on exceptions, 0 on success
+SELENIUM_BROWSER_URL=${SELENIUM_BROWSER_URL} ./run.sh --log-junit=logs/junit-${BUILD_NUMBER}.xml --testdox-html=logs/testdox-${BUILD_NUMBER}.html "$@"
+exit $?

834dda04f1adc29ee35892ab1f0b5286cf1380d3
medvedi <pliuwik@gmail.com>
2013-03-20 16:13:14 +0000

    Added sleep to wait for services to start.

--- selenium_start.sh
+++ selenium_start.sh
@@ -6,5 +6,10 @@ DISPLAY_NUMBER=80
 Xvfb :${DISPLAY_NUMBER} -screen 0 1024x768x24 2>&1 >/dev/null &
 echo $! > xvfb.pid
 
+sleep 2
+
 DISPLAY=:${DISPLAY_NUMBER} java -jar ${SELENIUM} 2>&1 >/dev/null &
 echo $! > selenium.pid
+
+sleep 3
+
